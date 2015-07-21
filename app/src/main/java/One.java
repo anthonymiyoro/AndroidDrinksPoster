b8c9e6e1c2aef917fdd629893d18256979339318
anthonymiyoro <amiyoro@gmail.com>
2015-07-21 18:02:59 +0300

    Linked Java files to XML files

--- app/src/main/java/One.java
+++ app/src/main/java/One.java
@@ -1,5 +1,16 @@
 /**
  * Created by tony on 21/07/15.
  */
-public class One {
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.blogapp.jaegers.R;
+
+public class One extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.one);
+    }
 }
--- app/src/main/java/Splash.java
+++ app/src/main/java/Splash.java
@@ -1,5 +1,16 @@
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.blogapp.jaegers.R;
+
 /**
  * Created by tony on 21/07/15.
  */
-public class Splash {
+public class Splash extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+    }
 }
