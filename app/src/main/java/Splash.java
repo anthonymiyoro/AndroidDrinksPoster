446dc2c3f0a31dc02dc236241c46cb84f53c8355
anthonymiyoro <amiyoro@gmail.com>
2015-07-22 15:28:17 +0300

    Added  gif call to Splash.java

--- app/src/main/java/Splash.java
+++ app/src/main/java/Splash.java
@@ -4,6 +4,8 @@ import android.view.SurfaceView;
 
 import com.blogapp.jaegers.R;
 
+import gif.decoder.GifRun;
+
 /**
  * Created by tony on 21/07/15.
  */
@@ -14,6 +16,9 @@ public class Splash extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash);
         SurfaceView v = (SurfaceView) findViewById(R.id.surfaceView1);
+
+        GifRun w = GifRun();
+        w.LoadGiff(v,this,R.drawable.giphy);
     }
 
 }
