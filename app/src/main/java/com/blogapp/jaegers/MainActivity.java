9d9f925e288035219b639b3a38b0c8e0a0f70750
anthonymiyoro <amiyoro@gmail.com>
2015-07-22 12:03:36 +0300

    Added a change page function to login button

--- app/src/main/java/com/blogapp/jaegers/MainActivity.java
+++ app/src/main/java/com/blogapp/jaegers/MainActivity.java
@@ -1,18 +1,34 @@
 package com.blogapp.jaegers;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
 ;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    public void jumpToPage(View view) {
+        mViewPager.setCurrentItem(1);
+    }
+
 }
+
+
 
 
